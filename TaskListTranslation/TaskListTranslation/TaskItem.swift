e7f4650eefd8fe470fb046769d0964d960f512c1
Peter Grapentien <a100933@qh623x3nf2.prog1.com>
2024-10-04 13:38:01 -0400

    Created basic task list

---
+++ TaskListTranslation/TaskListTranslation/TaskItem.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  TaskListTranslation
+//
+//  Created by Peter Grapentien on 10/4/24.
+//
+
---
+++ TaskListTranslation/TaskListTranslation/TaskListTranslationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListTranslationApp.swift
+//  TaskListTranslation
+//
+//  Created by Peter Grapentien on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskListTranslationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
